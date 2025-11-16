0b3b851dda3a9223080af1cb95f6ac7464a209c0
Max <malee@hamilton.edu>
2025-11-15 19:47:36 -0500

    added run.r

--- data/plumber.R
+++ data/plumber.R
@@ -4,15 +4,19 @@ library(plumber)
 
 source("data.R")
 
+#* Predict subway accessibility and rent data for a location
+#* @param lat Latitude of the location
+#* @param lng Longitude of the location
+#* @get /predict
 function(lat, lng) {
   lat <- as.numeric(lat)
   lng <- as.numeric(lng)
-
+  
   if (is.na(lat) || is.na(lng)) {
     return(list(error = "lat and lng must be numeric"))
   }
-
+  
   result <- run_model(lat, lng)
-
-  result
+  
+  return(result)
 }
